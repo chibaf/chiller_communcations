ac369364c1c868e2524e6f194e5344bce557cc3d
chibaf <chibaf@mac.com>
2023-09-13 18:37:57 +0900

    Add files via upload

--- set_temp.py
+++ set_temp.py
@@ -8,15 +8,27 @@ speed=9600
 timeout=1
 ser = serial.Serial(port,speed,timeout=1)
 
-request_00S1 = b'\x04\x30\x30\x02\x4A\x4F\x31\x03\x37'
+# get temperature
+request_00S1 = b'\x04\x30\x30\x53\x31\x05'
 ser.write(request_00S1)
 line = ser.readline()  
 print(line)
 line2 = line.strip().decode("utf-8")
 print(line2)
 
+print("###")
+
+# set temperetue=34.0C
 S1_25=b'\x04\x30\x30\x02\x53\x31\x20\x20\x20\x20\x33\x34\x2E\x30\x03\x78'
 ser.write(S1_25)
+line = ser.readline()  
+print(line)
+#line2 = line.strip().decode("utf-8")
+#print(line2) # return code from chiller
+
+# get temperature
+request_00S1 = b'\x04\x30\x30\x53\x31\x05'
+ser.write(request_00S1)
 line = ser.readline()  
 print(line)
 line2 = line.strip().decode("utf-8")
