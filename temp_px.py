0d3e0ef1adf8e30e1e2d74c24fa2fb0f2ff4477e
chibaf <chibaf@mac.com>
2023-09-14 12:23:38 +0900

    Add files via upload

--- temp_px.py
+++ temp_px.py
@@ -23,11 +23,26 @@ line2 = line.strip().decode("utf-8")
 b=line2.split()
 print(b[1][:-1])
 c=float(b[1][:-2])
-print(c+x)
+d=c+x
+print(d)
 print("###")
 
-# set temperetue=34.0C
-#S1_25=b'\x04\x30\x30\x02\x53\x31\x20\x20\x20\x20\x33\x34\x2E\x30\x03\x78'
+# set temperetue +=x
+# set_temp=b'\x04\x30\x30\x02\x53\x31\x20\x20\x20\x20\x33\x34\x2E\x30\x03\x78'
+t1=0x53^0x31^0x20^0x20^0x20^0x20
+print(t1)
+e=str(d)
+print(e)
+e0=ord(e[0])
+e1=ord(e[1])
+e2=ord(e[2])
+e3=ord(e[3])
+print(e0)
+print(e1)
+print(e2)
+print(e3)
+set_temp=t1^e0^e1^e2^e3^0x03
+print(hex(set_temp))
 #ser.write(S1_25)
 #line = ser.readline()  
 #print(line)
